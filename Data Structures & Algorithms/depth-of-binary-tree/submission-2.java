e74d5f30ccc41edab480245e56642d10dcf593a2
Nguyễn Tiến Nhật Nguyên <nhatnguyenmich1926@gmail.com>
2026-05-10 10:43:31 +0700

    Add: depth-of-binary-tree - submission-2

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-2.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        int depth = 0;
+
+        while (!q.isEmpty()){
+            int currentSize = q.size();
+
+            for (int i = 0; i < currentSize; i++){
+                TreeNode currentNode = q.poll();
+
+                if (currentNode.left != null) q.add(currentNode.left);
+                if (currentNode.right != null) q.add(currentNode.right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+}
