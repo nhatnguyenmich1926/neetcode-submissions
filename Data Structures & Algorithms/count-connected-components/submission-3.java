b2b24ab6fc64ae6b58da2ce605e441fb022cbc65
Nguyễn Tiến Nhật Nguyên <nhatnguyenmich1926@gmail.com>
2026-06-09 21:49:03 +0700

    Add: count-connected-components - submission-3

---
+++ Data Structures & Algorithms/count-connected-components/submission-3.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        ArrayList<ArrayList<Integer>> graph = create_graph(n,edges);
+
+        int count = 0; 
+        boolean[] visited = new boolean[n];
+
+        for (int i = 0; i < n; i++){
+            if (!visited[i]){
+                count++;
+                dfs_traversal(i,graph,visited);
+            }
+        }
+        return count;
+    }
+
+    public static ArrayList<ArrayList<Integer>> create_graph(int n, int [][] edges){
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0 ; i < n; i++){
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < edges.length; i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+        return graph;
+    }
+
+    public static void dfs_traversal(int start, ArrayList<ArrayList<Integer>> graph, boolean[] visited){
+        Stack<Integer> st = new Stack<>();
+        st.push(start);
+
+        while (!st.isEmpty()){
+            int current = st.pop();
+
+            if (!visited[current]){
+                visited[current] = true;
+
+                ArrayList<Integer> neighbors = graph.get(current);
+
+                for (int i = 0 ; i < neighbors.size(); i++){
+                    int next = neighbors.get(i);
+
+                    if (!visited[next]){
+                        st.push(next);
+                    }
+                }
+            }
+        }
+    }
+}
